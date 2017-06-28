0d175f07501daea4353228341874fcbb0992a123
Kory Dondzila <korydondzila@gmail.com>
2017-06-27 21:58:50 -0400

    Split into components but won't drag arfter some time.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import * as React from 'react';
 import * as d3 from 'd3';
 import './App.css';
 
-interface Props {
-  width: number;
-  height: number;
-  graph: Graph;
-}
-
 type d3Node = {
   id: string,
   group: number
@@ -24,10 +18,10 @@ type Graph = {
   links: d3Link[]
 };
 
-// interface NodeProps {
-//   id: string;
-//   group: string;
-// }
+interface NodeProps {
+  nodes: d3Node[];
+  simulation: any;
+}
 
 // class Node extends React.Component<NodeProps, {}> {
 //   render() {
@@ -39,39 +33,64 @@ type Graph = {
 //   }
 // }
 
-// class Link extends React.Component<{ value: number }, {}> {
-//   render() {
-//     return (
-//       <line strokeWidth={Math.sqrt(this.props.value)} />
-//     );
-//   }
-// }
-
-class App extends React.Component<Props, {}> {
-  divRef: HTMLDivElement;
-  //simulation: any;
+class Nodes extends React.Component<NodeProps, {}> {
+  ref: any;
+  simulation: any = this.props.simulation;
 
   componentDidMount() {
-    const context = this.setContext();
-    this.addNodesDragEvents(context);
-  }
-
-  setContext() {
-    return d3.select(this.divRef).select(".container");
-  }
-
-  addNodesDragEvents(context: any) {
+    const context: any = d3.select(this.ref);
     const color = d3.scaleOrdinal(d3.schemeCategory20);
+    const node = context.selectAll("circle")
+      .data(this.props.nodes)
+      .enter().append("circle")
+      .attr("r", 5)
+      .attr("fill", function(d: d3Node) {
+        return color(d.group.toString());
+      })
+      .call(d3.drag()
+          .on("start", onDragStart)
+          .on("drag", onDrag)
+          .on("end", onDragEnd));
 
-    const simulation: any = d3.forceSimulation()
-      .force("link", d3.forceLink().id(function(d: any) {
+    node.append("title")
+      .text(function(d: d3Node) {
         return d.id;
-      }))
-      .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(this.props.width / 2, this.props.height / 2));
+      });
 
-    var link = context.append("g")
-      .attr("class", "links")
+    function onDragStart(d: any) {
+      if (!d3.event.active) {
+        //this.simulation.alphaTarget(0.3).restart();
+      }
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function onDrag(d: any) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function onDragEnd(d: any) {
+      if (!d3.event.active) {
+        //this.simulation.alphaTarget(0);
+      }
+      d.fx = null;
+      d.fy = null;
+    }
+  }
+
+  render() {
+    return <g className="nodes" ref={(ref: any) => this.ref = ref}/>;
+  }
+}
+
+class Links extends React.Component<{graph: Graph, simulation: any}, {}> {
+  ref: any;
+  simulation: any = this.props.simulation;
+
+  componentDidMount() {
+    const context: any = d3.select(this.ref);
+    const link = context
       .selectAll("line")
       .data(this.props.graph.links)
       .enter().append("line")
@@ -79,54 +98,11 @@ class App extends React.Component<Props, {}> {
         return Math.sqrt(d.value);
       });
 
-    console.log("links", link);
+    const node = d3.select(".container").select(".nodes")
+      .selectAll("circle");
 
-    const node = context.append("g")
-      .attr("class", "nodes")
-      .selectAll("circle")
-      .data(this.props.graph.nodes)
-      .enter().append("circle")
-      .attr("r", 5)
-      .attr("fill", function(d: d3Node) {
-        return color(d.group.toString());
-      })
-      .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
+    this.simulation.nodes(this.props.graph.nodes).on("tick", ticked);
 
-    node.append("title")
-      .text(function(d: d3Node) {
-        return d.id;
-      });
-    // const nodes = d3.selectAll(".node").call(d3.drag()
-    //   .on("start", dragstarted)
-    //   .on("drag", dragged)
-    //   .on("end", dragended)
-    // );
-
-    function dragstarted(d: any) {
-      if (!d3.event.active) {
-        simulation.alphaTarget(0.3).restart();
-      }
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d: any) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d: any) {
-      if (!d3.event.active) {
-        simulation.alphaTarget(0);
-      }
-      d.fx = null;
-      d.fy = null;
-    }
-
-    console.log(node);
     function ticked() {
       link
         .attr("x1", function(d: any) {
@@ -150,44 +126,56 @@ class App extends React.Component<Props, {}> {
           return d.y;
         });
     }
-
-    simulation.nodes(this.props.graph.nodes).on("tick", ticked);
-    simulation.force("link").links(this.props.graph.links);
-    simulation.force("charge").strength(-100);
   }
 
   render() {
-    const { width, height } = this.props;
+    return <g className="links" ref={(ref: any) => this.ref = ref}/>;
+  }
+}
+
+// class Link extends React.Component<{ value: number }, {}> {
+//   render() {
+//     return (
+//       <line strokeWidth={Math.sqrt(this.props.value)} />
+//     );
+//   }
+// }
+
+interface Props {
+  width: number;
+  height: number;
+  graph: Graph;
+}
+
+class App extends React.Component<Props, {}> {
+  ref: HTMLDivElement;
+  simulation: any;
+
+  constructor(props: Props) {
+    super(props);
+    this.simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().id(function(d: any) {
+        return d.id;
+      }))
+      .force("charge", d3.forceManyBody().strength(-100))
+      .force("center", d3.forceCenter(this.props.width / 2, this.props.height / 2));
+
+    this.simulation.nodes(this.props.graph.nodes);
+    this.simulation.force("link").links(this.props.graph.links);
+  }
+
+  render() {
+    const { width, height, graph } = this.props;
     const style = {
       width,
       height
     };
 
-    //const color = d3.scaleOrdinal(d3.schemeCategory20);
-    // this.simulation = d3.forceSimulation()
-    //   .force("link", d3.forceLink().id(function(d: any) {
-    //     return d.id;
-    //   }))
-    //   .force("charge", d3.forceManyBody())
-    //   .force("center", d3.forceCenter(width / 2, height / 2));
-
-    // const links = graph.links.map((link: d3Link, index: number) => {
-    //   return <Link key={index} value={link.value}/>;
-    // });
-    
-    // const nodes = graph.nodes.map((node: d3Node, index: number) => {
-    //   return <Node key={index} id={node.id} group={color(node.group.toString())}/>;
-    // });
-
     return (
-      <div style={style} ref={(mountPoint: any) => this.divRef = mountPoint}>
+      <div style={style} ref={(ref: any) => this.ref = ref}>
         <svg className="container" width={width} height={height}>
-          {/*<g className="nodes">
-            {nodes}
-          </g>
-          <g className="links">
-            {links}
-          </g>*/}
+          <Nodes nodes={graph.nodes} simulation={this.simulation}/>
+          <Links graph={graph} simulation={this.simulation}/>
         </svg>
       </div>
     );
